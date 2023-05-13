dbe49a5028c8edebcf88383f25f4924687b4b7e0
LLopezPabon <127800002+LLopezPabon@users.noreply.github.com>
2023-05-12 19:08:26 -0500

    Create hola.js

---
+++ hola.js
@@ -0,0 +1,8 @@
+function hola(name) {
+    console.log("Hola" , name , "!")
+}
+
+console.log(hola("Linita")) // "Hola Linita!"
+
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
